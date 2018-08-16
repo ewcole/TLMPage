307faa637ae130d34c07cfce0cfc4e87fe4bd0b7
Ed Cole <edcole@mail.sunyjcc.edu>
2018-08-16 17:53:12 -0400

    Announcements

--- BuildHomePage.groovy
+++ BuildHomePage.groovy
@@ -8,7 +8,18 @@ def src = "templates pages announcement news".split(/\s+/).collect {[key: it, va
 }
 
 // Dig out the announcements
+def announcementTemplateFile = new File(src.templates, 'announcements.template.html');
+def annTemplate = new SimpleTemplateEngine()
+                    .createTemplate(announcementTemplateFile.text);
+def announcementSrc = src.announcement.list().grep {
+  (it =~ /^\d{4}-\d{2}-\d{2}.*\.xml$/)
+}.sort().collect{new File(src.announcement, it)};
 
+def announcementList = announcementSrc.collect {
+  announcementFile ->
+  new XmlSlurper().parse(announcementFile);
+}
+String announcements = annTemplate.make([announcements: announcementList]);
 // Create the news
 
 // Create the home page
@@ -20,7 +31,7 @@ def indexTemplate = new SimpleTemplateEngine()
 
 indexFile.withWriter {
   w ->
-  indexTemplate.make([announcements: "",
+  indexTemplate.make([announcements: announcementList,
                       news: ""]).toString().readLines().each {
     w.println it
     // println it;
